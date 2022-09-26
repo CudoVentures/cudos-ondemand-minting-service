23684ecec0359046c1346307a480ae432d5b18a3
avalkov <avalkov@mail.bg>
2022-09-26 13:12:03 +0300

    Refactor encoding config

--- cmd/cudos-ondemand-minting-service/main.go
+++ cmd/cudos-ondemand-minting-service/main.go
@@ -5,13 +5,11 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/cosmos/cosmos-sdk/simapp/params"
-	"github.com/cosmos/cosmos-sdk/std"
-	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/rs/zerolog/log"
 
 	cudosapp "github.com/CudoVentures/cudos-node/app"
 	"github.com/CudoVentures/cudos-ondemand-minting-service/internal/config"
+	encodingconfig "github.com/CudoVentures/cudos-ondemand-minting-service/internal/encoding_config"
 	key "github.com/CudoVentures/cudos-ondemand-minting-service/internal/key"
 	relayminter "github.com/CudoVentures/cudos-ondemand-minting-service/internal/relay_minter"
 	filestate "github.com/CudoVentures/cudos-ondemand-minting-service/internal/state/file"
@@ -26,7 +24,7 @@ func main() {
 	}
 
 	cudosapp.SetConfig()
-	encodingConfig := makeEncodingConfig([]module.BasicManager{cudosapp.ModuleBasics})()
+	encodingConfig := encodingconfig.MakeEncodingConfig()
 
 	state := filestate.NewFileState(cfg.StateFile)
 
@@ -43,51 +41,4 @@ func main() {
 	err = minter.Start(context.Background())
 
 	log.Fatal().Err(err).Send()
-}
-
-// func initializeEncodingConfig() (params.EncodingConfig, error) {
-// dir, err := ioutil.TempDir("", "ondemand-minting")
-// if err != nil {
-// 	return params.EncodingConfig{}, err
-// }
-
-// db, err := sdk.NewLevelDB("ondemand-minting", dir)
-// if err != nil {
-// 	return params.EncodingConfig{}, err
-// }
-
-// userHomeDir, err := os.Getwd()
-// if err != nil {
-// 	return params.EncodingConfig{}, err
-// }
-
-// defaultNodeHome := filepath.Join(userHomeDir, "cudos-data")
-
-// TODO: Replace with proper logger
-// _ = simapp.NewSimApp(tendermintlogger.NewNopLogger(), db, nil, true, map[int64]bool{}, defaultNodeHome, sdksimapp.FlagPeriodValue, encodingConfig, simapp.EmptyAppOptions{})
-
-// 	return encodingConfig, nil
-// }
-
-func makeEncodingConfig(managers []module.BasicManager) func() params.EncodingConfig {
-	return func() params.EncodingConfig {
-		encodingConfig := params.MakeTestEncodingConfig()
-		std.RegisterLegacyAminoCodec(encodingConfig.Amino)
-		std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
-		manager := mergeBasicManagers(managers)
-		manager.RegisterLegacyAminoCodec(encodingConfig.Amino)
-		manager.RegisterInterfaces(encodingConfig.InterfaceRegistry)
-		return encodingConfig
-	}
-}
-
-// mergeBasicManagers merges the given managers into a single module.BasicManager
-func mergeBasicManagers(managers []module.BasicManager) module.BasicManager {
-	var union = module.BasicManager{}
-	for _, manager := range managers {
-		for k, v := range manager {
-			union[k] = v
-		}
-	}
-	return union
 }
---
+++ internal/encoding_config/encoding_config.go
@@ -0,0 +1,29 @@
+package encodingconfig
+
+import (
+	cudosapp "github.com/CudoVentures/cudos-node/app"
+	"github.com/cosmos/cosmos-sdk/simapp/params"
+	"github.com/cosmos/cosmos-sdk/std"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+func MakeEncodingConfig() params.EncodingConfig {
+	encodingConfig := params.MakeTestEncodingConfig()
+	std.RegisterLegacyAminoCodec(encodingConfig.Amino)
+	std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	manager := mergeBasicManagers([]module.BasicManager{cudosapp.ModuleBasics})
+	manager.RegisterLegacyAminoCodec(encodingConfig.Amino)
+	manager.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	return encodingConfig
+}
+
+// mergeBasicManagers merges the given managers into a single module.BasicManager
+func mergeBasicManagers(managers []module.BasicManager) module.BasicManager {
+	var union = module.BasicManager{}
+	for _, manager := range managers {
+		for k, v := range manager {
+			union[k] = v
+		}
+	}
+	return union
+}
