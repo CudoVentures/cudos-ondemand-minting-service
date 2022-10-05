8f8bc5d7fd64c46eb5a08f655d177382abb81516
avalkov <avalkov@mail.bg>
2022-10-05 15:10:45 +0300

    Add Dockerfile for tests

--- integration_tests/setup_node.sh
+++ integration_tests/setup_node.sh
@@ -11,6 +11,21 @@ cd $WORKDIR
 cp $WORKDIR'/root-node.local.env' $WORKDIR'/cudos-builders/docker/root-node/root-node.local.env'
 
 cd $WORKDIR'/cudos-builders/tools-nodejs/init-local-node-without-docker'
+
 chmod +x init.sh
 source ./init.sh 2>node.output &
+
+echo "WILL GET SOME SLEEP"
+sleep 30
+echo "WILL CHECK THE OUTPUT"
+
+echo "CUDOS HOME OUTPUT"
+echo $CUDOS_HOME
+
+echo "WILL OUTPUT APP.TOML CONFIG"
+cat ./cudos-data/config/app.toml
+
+echo "WILL OUTPUT APP.TOML CONFIG VIA CUDOS_HOME"
+cat $CUDOS_HOME'/cudos-data/config.toml'
+
 cd $WORKDIR
