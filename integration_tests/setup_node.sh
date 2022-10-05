b8cb1691a755eb5525d959543471f13d8600edfd
avalkov <avalkov@mail.bg>
2022-10-05 15:10:45 +0300

    Add Dockerfile for tests

--- integration_tests/setup_node.sh
+++ integration_tests/setup_node.sh
@@ -10,7 +10,11 @@ make install
 cd $WORKDIR
 cp $WORKDIR'/root-node.local.env' $WORKDIR'/cudos-builders/docker/root-node/root-node.local.env'
 
+sed -i 's/keyring-backend os/keyring-backend test/g' $WORKDIR'/cudos-builders/docker/root-node/scripts/init-root.sh'
+
 cd $WORKDIR'/cudos-builders/tools-nodejs/init-local-node-without-docker'
+
 chmod +x init.sh
 source ./init.sh 2>node.output &
+
 cd $WORKDIR
