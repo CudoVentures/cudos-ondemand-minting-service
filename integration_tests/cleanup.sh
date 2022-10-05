699bd2c58f7f153c6d41a5b0469876acbd739cb9
avalkov <avalkov@mail.bg>
2022-10-05 12:35:38 +0300

    Setup CI

--- integration_tests/cleanup.sh
+++ integration_tests/cleanup.sh
@@ -1,7 +1,10 @@
 pkill -f cudos-ondemand-minting-service
 pkill -f mock-service
+pkill -f cudos-noded
 
 rm cudos-ondemand-minting-service.test
 rm cudos-ondemand-minting-service.out
-rm unittests.out
+rm unittests1.out
+rm unittests2.out
+rm unittests3.out
 rm merged.cov
---
+++ integration_tests/start.sh
@@ -0,0 +1,10 @@
+cd ./integration_tests
+
+source ./setup_node.sh
+
+# TODO: Have smarter way to wait make sure that node is running
+sleep 30
+
+cd ..
+source ./integration_tests/tests.sh
+source ./integration_tests/cleanup.sh
