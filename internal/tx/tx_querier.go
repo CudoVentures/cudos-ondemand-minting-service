ed6959f4eb6859e606d1ef62dea5d3cedc408e80
Kamen Stoykov <k.stoykov@razorlabs.com>
2023-03-22 12:45:55 +0000

    update txQuery to fetch all events

--- internal/tx/tx_querier.go
+++ internal/tx/tx_querier.go
@@ -3,7 +3,6 @@ package tx
 import (
 	"context"
 	"fmt"
-	"time"
 
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 )
@@ -13,16 +12,42 @@ func NewTxQuerier(node txSearcher) *txQuerier {
 }
 
 func (tq *txQuerier) Query(ctx context.Context, query string) (*ctypes.ResultTxSearch, error) {
-	// TODO: Do we need to do pagination? Will we get all results if we don't use pagination or there is some limit?
-	results, err := tq.node.TxSearch(ctx, query, true, nil, nil, "asc")
-	if err != nil {
-		return nil, fmt.Errorf("tx search (%s) failed: %s", query, err)
+	var allResults *ctypes.ResultTxSearch = nil
+
+	for page := 1; ; page += 1 {
+		// fmt.Printf("Fetching page %d\n", page)
+		results, err := tq.node.TxSearch(ctx, query, true, &page, &itemsPerPage, "asc")
+		if err != nil {
+			return nil, fmt.Errorf("tx search (%s) failed: %s", query, err)
+			// if allResults == nil {
+			// 	return nil, fmt.Errorf("tx search (%s) failed: %s", query, err)
+			// } else {
+			// 	fmt.Println(err)
+			// 	continue
+			// }
+		}
+
+		if allResults == nil {
+			allResults = results
+		} else {
+			allResults.Txs = append(allResults.Txs, results.Txs...)
+			allResults.TotalCount = results.TotalCount
+		}
+
+		from := (page - 1) * itemsPerPage
+		to := page*itemsPerPage - 1
+		// fmt.Printf("[%d; %d] -> %d\n", from, to, allResults.TotalCount)
+		if from <= allResults.TotalCount && allResults.TotalCount <= to {
+			break
+		}
 	}
 
-	return results, nil
+	return allResults, nil
 }
 
-const txSearchTimeout = 10 * time.Second
+// const txSearchTimeout = 10 * time.Second
+
+var itemsPerPage = 100
 
 type txQuerier struct {
 	node txSearcher
